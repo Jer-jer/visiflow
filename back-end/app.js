c40011bb77892b7cca2bc98153ef54d26753ddab
Neil Collado <colladoneil326@gmail.com>
2023-09-12 22:29:06 +0800

    Added Backend code (#4)
    Co-authored-by: Neil Collado <colladon@touch-fire.com>

---
+++ back-end/app.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+const express = require('express');
+const expressLayout = require('express-ejs-layouts');
+
+const connectDB = require('./server/config/db');
+
+const app = express();
+const PORT = 5000 || process.env.PORT;
+
+//connect to DB
+connectDB();
+
+
+app.use(express.static('public'));
+
+//Templating engine
+app.use(expressLayout);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+app.use('/', require('./server/routes/main'));
+
+app.listen(PORT, () => {
+    console.log('App listening on port ${PORT}');
+})
