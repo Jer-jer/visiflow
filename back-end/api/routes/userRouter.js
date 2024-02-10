4551243cb67f2ca4d9fa6dbed55bdf0d3b84c7f1
Neil Collado <colladoneil326@gmail.com>
2024-02-10 14:52:08 +0800

    Neil be branch (#48)
    * Controller and Middleware Update
    
    Updated the UserController and created a middleware for UserValidation and sanitizeData
    
    * Visitor Controller Update
    
    Updated visitor controller crud and model
    
    * Updated Companion
    
    Updated companion controller and model
    
    * Added Authentication
    
    * Updates for authentication
    
    * Updated user models
    
    * fe-updates

--- back-end/api/routes/userRouter.js
+++ back-end/api/routes/userRouter.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 
-// router.use((req, res, next) => {
-//     if(req.user) next();
-//     else res.send(401);
-// });
+router.use((req, res, next) => {
+    if(req.user) next();
+    else res.send(401);
+});
 
 router.get("/", userController.getAllUsers);
 
