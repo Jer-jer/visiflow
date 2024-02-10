2dbd04af4a57c1fd0326a2238d5af6ec6664a65d
Neil Collado <colladoneil326@gmail.com>
2024-02-10 13:52:09 +0800

    Neil be branch (#45)
    * Controller and Middleware Update
    
    Updated the UserController and created a middleware for UserValidation and sanitizeData
    
    * Visitor Controller Update
    
    Updated visitor controller crud and model
    
    * Updated Companion
    
    Updated companion controller and model
    
    * Added Authentication
    
    * Updates for authentication
    
    * Updated user models

--- back-end/api/utils/sessionHelper.js
+++ back-end/api/utils/sessionHelper.js
@@ -7,19 +7,11 @@ const MongoStore = require('connect-mongo');
 // const sessionSecret = crypto.randomBytes(32).toString('hex');
 
 const sessionMiddleware = session({
-<<<<<<< HEAD
         secret: '292e7192c6b4d32343d726c51f2ccab51fadcbdf91f48695d24d23d307e25c70',
         resave: false,
         saveUninitialized: false,
         store: MongoStore.create({
                 mongoUrl: process.env.MONGODB_URI + '/auth',
-=======
-        secret: 'aksndlkajsdlkansld',
-        resave: false,
-        saveUninitialized: false,
-        store: MongoStore.create({
-                mongoUrl: process.env.MONGODB_URI,
->>>>>>> master
         }),
 });
 
