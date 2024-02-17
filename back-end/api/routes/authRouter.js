f5833ff87bc4e1f0f2178eabd1fd4767107ec65a
Neil Collado <colladoneil326@gmail.com>
2024-02-17 15:56:32 +0800

    Added JWT Token (#52)
    * Controller and Middleware Update
    
    Updated the UserController and created a middleware for UserValidation and sanitizeData
    
    * Visitor Controller Update
    
    Updated visitor controller crud and model
    
    * Updated Companion
    
    Updated companion controller and model
    
    * Added Authentication
    
    * Updates for authentication
    
    * Updated user models
    
    * fe-updates
    
    * Updated validation
    
    * Added JWT Token

--- back-end/api/routes/authRouter.js
+++ back-end/api/routes/authRouter.js
@@ -4,8 +4,10 @@ const passport = require('passport');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-router.post('/login', passport.authenticate('local'), authController.login);
+router.post('/login', passport.authenticate('local', {session: false}), authController.login);
 
 router.get('/logout', authController.logout);
+
+router.get('/test', passport.authenticate('jwt', { session: false }), authController.test);
 
 module.exports = router;
