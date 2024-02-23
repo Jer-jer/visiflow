823b1e38c51481ec9c52b45d32d90dbeb778b3ce
Neil Collado <colladoneil326@gmail.com>
2024-02-23 16:05:44 +0800

    Addded refresh token

--- back-end/api/routes/userRouter.js
+++ back-end/api/routes/userRouter.js
@@ -3,10 +3,10 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 
-router.use((req, res, next) => {
-    if(req.user && req.user.role.includes("admin")) next();
-    else res.send(401);
-});
+// router.use((req, res, next) => {
+//     if(req.user && req.user.role.includes("admin")) next();
+//     else res.send(401);
+// });
 
 router.get("/", userController.getAllUsers);
 
