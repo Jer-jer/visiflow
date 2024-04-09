9e7778b757fa6e6ce49a309d972ecbb44e2ad8ae
Todd Machacon <18103195@usc.edu.ph>
2024-04-10 00:21:17 +0800

    Merging

--- back-end/api/routes/employeesRouter.js
+++ back-end/api/routes/employeesRouter.js
@@ -1,32 +1,14 @@
 const express = require("express");
 const router = express.Router();
-<<<<<<< HEAD
 const employeesController = require("../controllers/employeesController");
-=======
-const announcementsController = require("../controllers/employeesController");
->>>>>>> master
 
 // router.use((req, res, next) => {
 //   if (req.user && req.user.role.includes("admin")) next();
 //   else res.send(401);
 // });
-<<<<<<< HEAD
 router.get("/", employeesController.getAllEmployees);
 router.post("/new", employeesController.createNewEmployees);
 router.post("/search", employeesController.getEmployeesByName);
 router.put("/update", employeesController.updateEmployees);
 router.delete("/delete", employeesController.deleteEmployees);
-=======
-
-router.get("/", announcementsController.getAllEmployees);
-
-router.post("/new", announcementsController.createNewEmployees);
-
-// router.post("/search", announcementsController.getEmployeesByName);
-
-// router.put("/update", announcementsController.updateEmployees);
-
-// router.delete("/delete", announcementsController.deleteEmployees);
-
->>>>>>> master
 module.exports = router;
