e017913a0516383816c815f3936a71d757da90bd
Todd Machacon <18103195@usc.edu.ph>
2024-04-10 16:22:33 +0800

    merge

--- front-end/visitor-system/src/components/google-translate/index.tsx
+++ front-end/visitor-system/src/components/google-translate/index.tsx
@@ -1,11 +1,14 @@
 import "./styles.scss";
+
 import React, { useEffect } from 'react';
 
 
-const GoogleTranslateComponent: React.FC = () => {
+const GoogleTranslateComponent: React.FC = () =>{
   
     return (
+      <div>
         <div id="google_translate_element"></div>
+      </div>
     );
 }
 
