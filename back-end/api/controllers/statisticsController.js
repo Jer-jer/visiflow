fb40f68410c96a58094cd348113bec84765a607f
Neil Collado <colladoneil326@gmail.com>
2024-04-16 19:25:01 +0800

    Neil be branch (#132)
    * Controller and Middleware Update
    
    Updated the UserController and created a middleware for UserValidation and sanitizeData
    
    * Visitor Controller Update
    
    Updated visitor controller crud and model
    
    * Updated Companion
    
    Updated companion controller and model
    
    * Added Authentication
    
    * Updates for authentication
    
    * Updated user models
    
    * fe-updates
    
    * Updated validation
    
    * Added JWT Token
    
    * Addded refresh token
    
    * be code cleanup
    
    * be code cleanup
    
    * added gitignore to sensitive data
    
    * added QR code to pre-reg
    
    * added upload photo to gcs
    
    * fix datepicker null to undefined
    
    * base x64 upload
    
    * rollback
    
    * added qr code and email to companion
    
    * added instant notification
    
    * added notification endpoints
    
    * added walk in time in
    
    * added instant notifcation via socket.io
    
    * fixed merge conflict
    
    * custom objectId for visitor
    
    * custom objectId for visitor
    
    * fixed the check badge
    
    * fixed notification for time in & out
    
    * added systemlog
    
    * added system logs
    
    * fixed system logs
    
    * new update
    
    * fixed systemlog
    
    * fix
    
    * fix
    
    * code cleanup for visitor module
    
    * fix
    
    * fixed unauthorized badge
    
    * consolidated helper functions
    
    * fixed unauthorized user
    
    * fixed validation and duplicate validation
    
    * fixed validation and duplicate validation
    
    * fixed QR Badge errors
    
    * added notify POI
    
    * fix
    
    * improved badge generation
    
    * improved badge generation
    
    * initial statistics
    
    * aggregation

--- back-end/api/controllers/statisticsController.js
+++ back-end/api/controllers/statisticsController.js
@@ -2,7 +2,7 @@ const Visitor = require('../models/visitor');
 const Badge = require('../models/badge');
 const Logs = require('../models/visitorLogs');
 
-const { 
+const {
   getVisitors,
   getVisitorList
 } = require('../utils/statUtils');
@@ -11,25 +11,25 @@ exports.totalVisitors = async (req, res) => {
   const { startDate, endDate } = req.body;
 
   try {
-    const {visitors, errors} = await getVisitors(startDate, endDate);
-  
+    const { visitors, errors } = await getVisitors(startDate, endDate);
+
     if (!(errors.length === 0)) {
       return res.status(400).json({ error: errors[0] });
     }
 
-    // return res.json({ visitors });
-
-    const total = visitors.reduce((total, visitor) => total += visitor.count, 0); 
+    const total = visitors.reduce((total, visitor) => total += visitor.count, 0);
     const percent = Math.ceil(visitors[0].count / total * 100);
     const walk_in = visitors.find(v => v._id === 'Walk-In');
     const pre_reg = visitors.find(v => v._id === 'Pre-Registered');
     const type = visitors[0]._id;
-    return res.status(200).json({ walk_in: walk_in.count, pre_reg: pre_reg.count, total: total, percent: `${percent}%`, type: type });
+
+
+    return res.status(200).json({ walk_in: walk_in ? walk_in.count : 0, pre_reg: pre_reg ? pre_reg.count : 0, total: total, percent: `${percent}%`, type: type });
 
   } catch (error) {
     return res.status(500).json({ error: 'Failed to retrieve visitors from the database.' });
   }
-} 
+}
 
 exports.mostVisited = async (req, res) => {
   const { startDate, endDate } = req.body;
@@ -66,7 +66,7 @@ exports.mostVisited = async (req, res) => {
         }
       }
     ]);
-    
+
     const who = await Visitor.aggregate([
       {
         $match: {
@@ -134,9 +134,85 @@ exports.mostVisited = async (req, res) => {
     ]);
 
 
-    return res.status(200).json({ what: what[0], where: where[0], when: when[0], who: who[0],  });
+    return res.status(200).json({ what: what[0], where: where[0], when: when[0], who: who[0], });
 
   } catch (error) {
     return res.status(500).json({ error: 'Failed to retrieve visitors from the database.' });
+  }
+}
+
+// exports.graph = async (req, res) => {
+//   const { month, year } = req.body;
+
+//   try {
+
+
+
+
+//   } catch (error) {
+//     return res.status(500).json({ error: 'Failed to retrieve visitors from the database.' });
+//   }
+// }
+
+exports.getYears = async (req, res) => {
+  try {
+    const yearsAndMonths = await Logs.aggregate([
+      {
+        $project: {
+          year: { $substr: ["$created_at", 0, 4] },
+          month: { $substr: ["$created_at", 5, 2] }
+        }
+      },
+      {
+        $group: {
+          _id: null,
+          years: { $addToSet: "$year" }, // Collect unique years
+          months: { $addToSet: "$month" } // Collect unique months
+        }
+      }
+    ]);
+
+    const years = yearsAndMonths[0].years.map(year => ({ _id: year }));
+    const months = yearsAndMonths[0].months.map(month => ({ _id: month }));
+
+    const _years = years.map(year => year._id)
+    const _months = months.map(month => month._id)
+
+    return res.json({ _years, _months });
+  } catch (error) {
+    return res.status(500).json({ error: 'Failed to retrieve years from the database.' });
+  }
+}
+
+exports.getWeeks = async (req, res) => {
+  try {
+
+    const weeks = await Logs.aggregate([
+      {
+        $project: {
+          month: { $substr: ["$created_at", 5, 2] },
+          day: { $substr: ["$created_at", 8, 2] }
+        }
+      },
+      {
+        $group: {
+          _id: "$month",
+          weekSet: {
+            $addToSet: {
+              _id: "$day",
+              week1: { $cond: [{ $and: [{ $gte: ["$day", "01"] }, { $lte: ["$day", "07"] }] }, true, false] },
+              week2: { $cond: [{ $and: [{ $gte: ["$day", "08"] }, { $lte: ["$day", "14"] }] }, true, false] },
+              week3: { $cond: [{ $and: [{ $gte: ["$day", "15"] }, { $lte: ["$day", "21"] }] }, true, false] },
+              week4: { $cond: [{ $and: [{ $gte: ["$day", "22"] }, { $lte: ["$day", "31"] }] }, true, false] }
+            }
+          }
+        }
+      }
+    ]);
+
+    return res.json({ weeks })
+
+  } catch (error) {
+    return res.status(500).json({ error: 'Failed to retrieve years from the database.' });
   }
 }
--- back-end/api/routes/statisticsRouter.js
+++ back-end/api/routes/statisticsRouter.js
@@ -6,4 +6,10 @@ router.post('/', StatsController.totalVisitors);
 
 router.post('/location', StatsController.mostVisited);
 
+// router.post('/graph', StatsController.graph);
+
+router.get('/getYears', StatsController.getYears);
+
+router.get('/getWeeks', StatsController.getWeeks);
+
 module.exports = router;
