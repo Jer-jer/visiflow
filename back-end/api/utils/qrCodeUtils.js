0982fa7bb9b308feaa69ed1b93e3a4758da0877b
Allan Jericho Bargamento <jerichoallan0@gmail.com>
2024-04-30 16:22:26 +0800

    fix 2

--- back-end/api/utils/qrCodeUtils.js
+++ back-end/api/utils/qrCodeUtils.js
@@ -99,7 +99,7 @@ async function generateBadge(visitor) {
     await badge.save();
 
     const filename = `badge${badge._id}.png`;
-    const uri = `https://${local_ip}/badge/checkBadge?qr_id=${visitor._id}`;
+    const uri = `${local_ip}/badge/checkBadge?qr_id=${visitor._id}`;
     await generateQRCode(uri, filename, badge._id);
 
     return badge;
