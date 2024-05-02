e3c2f7f1f210ca27c57331f649515d0310c2297b
Neil Collado <colladoneil326@gmail.com>
2024-05-02 16:57:50 +0800

    merge

--- back-end/server.js
+++ back-end/server.js
@@ -39,8 +39,9 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: [
-      "https://gullas-visiflow.onrender.com",
-      "https://gullas-visiflow-internal.onrender.com",
+      // "https://gullas-visiflow.onrender.com",
+      // "https://gullas-visiflow-internal.onrender.com",
+      "*"
     ],
   },
 });
